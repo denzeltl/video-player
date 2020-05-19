a69cf4c46f6e420e2fe2112a29853fb94340c6ff
Denzel <denzeltlee@gmail.com>
2020-05-19 22:29:05 +0800

    Updates

---
+++ precache-manifest.bf99ae3bd47526552e011b214854684f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37dc86a90871e68c9b0d2ae24dedb0c",
+    "url": "/video-player/index.html"
+  },
+  {
+    "revision": "be046b7ea2b6cd5d4d06",
+    "url": "/video-player/static/css/main.a4adda26.chunk.css"
+  },
+  {
+    "revision": "cfa5e3c123967bc56e3d",
+    "url": "/video-player/static/js/2.35e05995.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-player/static/js/2.35e05995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be046b7ea2b6cd5d4d06",
+    "url": "/video-player/static/js/main.5daf0040.chunk.js"
+  },
+  {
+    "revision": "7b70f8f21bc91a6290db",
+    "url": "/video-player/static/js/runtime-main.bde6eec1.js"
+  }
+]);
